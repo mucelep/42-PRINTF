f24276fc6081d9d17de33dafa54d829569a31c66
Muhammed Omer Celep <mucelep@k1m27s10.42istanbul.com.tr>
2026-02-14 14:38:19 +0300

    garip hata düzeltildi

--- ft_printf.c
+++ ft_printf.c
@@ -3,68 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-/*   By: mucelep <celepm82@gmail.com>               +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/10 21:01:03 by mucelep           #+#    #+#             */
-/*   Updated: 2026/02/10 21:01:03 by mucelep          ###   ########.fr       */
-=======
 /*   By: mucelep <mucelep@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 17:58:33 by mucelep           #+#    #+#             */
-/*   Updated: 2026/02/14 03:26:49 by mucelep          ###   ########.fr       */
->>>>>>> cbdd5f8 (hersey tamam)
+/*   Updated: 2026/02/14 14:37:31 by mucelep          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-<<<<<<< HEAD
-static int ft_format(char format, va_list vargs)
-=======
 static int	ft_format(char format, va_list vargs)
->>>>>>> cbdd5f8 (hersey tamam)
 {
 	if (format == 'c')
 		return (ft_putchar(va_arg(vargs, int)));
 	if (format == 's')
-<<<<<<< HEAD
-		return(ft_putstr(va_arg(vargs, char*)));
-	if (format == 'p')
-		return(ft_address(va_arg(vargs, void *)));
-	if (format == 'd' || format == 'i')
-		return(ft_putnbr(va_arg(vargs, int)));
-	if (format == 'u')
-		return(ft_unputnbr(va_arg(vargs, unsigned int)));
-	if (format == 'x')
-		return(ft_hexa(va_arg(vargs, unsigned int),"0123456789abcdef"));
-	if (format == 'X')
-		return(ft_hexa(va_arg(vargs, unsigned int),"0123456789ABCDEF"));
-	if (format == '%')
-		return(ft_putchar('%'));
-	return(-1);
-}
-
-int ft_printf(const char *format, ...)
-{
-	va_list vargs;
-	int count;
-	int check;
-
-	count = 0;
-	va_start(vargs,format);
-	while (*format)
-	{
-		if(*format == '%' && *(format + 1) != '\0')
-		{
-			check = ft_format(*(++format),vargs);
-			if (check == -1)
-				return (-1);
-			count += check;
-		}
-		else
-			count += ft_putchar(*format);
-=======
 		return (ft_putstr(va_arg(vargs, char *)));
 	if (format == 'p')
 		return (ft_address(va_arg(vargs, void *)));
@@ -104,18 +56,8 @@ int	ft_printf(const char *format, ...)
 		if (controller == -1)
 			return (-1);
 		count += controller;
->>>>>>> cbdd5f8 (hersey tamam)
 		format++;
 	}
 	va_end(vargs);
 	return (count);
 }
-<<<<<<< HEAD
-#include <stdio.h>
-int main ()
-{
-	ft_printf("%%\n");
-	printf("%%");
-}
-=======
->>>>>>> cbdd5f8 (hersey tamam)
--- ft_printf.h
+++ ft_printf.h
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-/*   By: mucelep <celepm82@gmail.com>               +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/10 21:01:08 by mucelep           #+#    #+#             */
-/*   Updated: 2026/02/10 21:01:08 by mucelep          ###   ########.fr       */
-=======
 /*   By: mucelep <mucelep@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 21:01:08 by mucelep           #+#    #+#             */
-/*   Updated: 2026/02/14 02:02:54 by mucelep          ###   ########.fr       */
->>>>>>> cbdd5f8 (hersey tamam)
+/*   Updated: 2026/02/14 14:37:30 by mucelep          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +16,6 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-<<<<<<< HEAD
-int ft_printf(const char *format, ...);
-int ft_hexa(unsigned long nb, char *base);
-int ft_unputnbr(unsigned int nb);
-int ft_putchar(char c);
-int ft_putstr(char *s);
-int ft_putnbr(long n);
-int ft_address(void *p);
-
-#endif
-=======
 int	ft_printf(const char *format, ...);
 int	ft_hexa(unsigned long nb, char *base);
 int	ft_unputnbr(unsigned int nb);
@@ -43,4 +25,3 @@ int	ft_putnbr(long n);
 int	ft_address(void *p);
 
 #endif
->>>>>>> cbdd5f8 (hersey tamam)
